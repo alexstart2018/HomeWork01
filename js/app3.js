c1f12ffe202870c1249a2a58a9e3189de91d0ac5
alexstart2018 <bpa.regist@gmail.com>
2018-08-10 00:40:24 +0300

    alm don

---
+++ js/app3.js
@@ -0,0 +1,27 @@
+'use strict';
+let a = prompt('Введите номер дня недели',"");
+switch (a) {
+    case '1':
+        alert( 'Понедельник' );
+        break;
+    case '2':
+        alert( 'Вторник' );
+        break;
+    case '3':
+        alert( 'Среда' );
+        break;
+    case '4':
+        alert( 'Четверг' );
+        break;
+    case '5':
+        alert( 'Пятница' );
+        break;
+    case '6':
+        alert( 'Суббота' );
+        break;
+    case '7':
+        alert( 'Воскресение' );
+        break;
+    default:
+        alert( 'Такого дня недели не существует!' )
+}

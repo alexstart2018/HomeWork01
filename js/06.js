30d8e3e2c6ca450d5687e5d46de800650484f19f
alexstart2018 <Overthetop11>
2018-08-13 17:29:54 +0300

    added

---
+++ js/06.js
@@ -0,0 +1,27 @@
+'use strict';
+//1
+let num1 = 4;
+while (num1 <= 400) {
+    console.log(++num1);
+    }
+//2
+let num2 = 1;
+while (num2 < 13) {
+    console.log(num2 += 3);
+}
+//3
+let num3 = 655;
+while (num3 > 0) {
+    console.log(--num3);
+}
+//4
+let num4 = 1982;
+while (num4 < 2017) {
+    console.log(++num4);
+}
+//5
+let num5 = -5;
+while (num5 < 100) {
+    console.log(++num5);
+}
+//6
